fe8393238029af07ca91c95ad4b564f0cb19f8cc
Simran Jaggi <Simran.Jaggi@Geminisolutions.com>
2026-07-14 11:13:09 +0530

    Add frontend files

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+//useState is a React Hook that lets you add and manage state in a functional component.
+//State is data that can change over time. When the state changes, 
+// React automatically re-renders the component to display the updated data.
+
+function App() {
+  const [issues, setIssues] = useState([]);
+  const [issueId, setIssueId] = useState("");
+
+  async function getIssues() {
+    const response = await fetch("http://localhost:8000/issues");
+    const data = await response.json();
+    setIssues(data);
+  }
+
+  async function searchIssue() {
+    const response = await fetch(
+      `http://localhost:8000/issues/${issueId}`
+    );
+
+    const data = await response.json();
+    setIssues([data]);
+  }
+
+  return (
+    <div style={{ padding: "30px" }}>
+      <h2>Jira Issues</h2>
+
+      <button onClick={getIssues}>Get All Issues</button>
+
+      <br />
+      <br />
+
+      <input
+        placeholder="Enter Issue ID"
+        value={issueId}
+        onChange={(e) => setIssueId(e.target.value)}
+      />
+
+      <button onClick={searchIssue}>Search</button>
+
+      <hr />
+
+      {issues.map((issue, index) => (
+        <div key={index}>
+          <h3>{issue.id}</h3>
+          <p>{issue.title}</p>
+          <p>{issue.description}</p>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
